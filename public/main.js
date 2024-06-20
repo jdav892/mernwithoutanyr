7f2a13830fcc5f37f199512a38bcc4d78fec2228
jdav892 <jadavila9@gmail.com>
2024-06-20 16:03:52 -0400

    update and delete work

--- public/main.js
+++ public/main.js
@@ -1,4 +1,7 @@
 const update = document.querySelector('#update-button')
+const deleteButton = document.querySelector('#delete-button')
+const messageDiv = document.querySelector("#message")
+
 
 update.addEventListener('click', _ => {
    fetch('/quotes', {
@@ -8,5 +11,32 @@ update.addEventListener('click', _ => {
         name: 'Darth Vader',
         quote: 'I find your lack of faith disturbing.',
     }),
-   }) 
+   })
+   .then(res => {
+    if(res.ok) return res.json()
+   })
+   .then(response => {
+    window.location.reload(true)
+   })
+})
+
+deleteButton.addEventListener('click', _ => {
+    fetch('/quotes', {
+        method: 'delete',
+        headers: { 'Content-Type' : 'application/json'},
+        body: JSON.stringify({
+            name: 'Darth Vader'
+        })
+    })
+    .then(res => {
+        if(res.ok) return res.json()
+    })
+    .then(response => {
+        if(response === 'No quotes to delete'){
+            messageDiv.textContent = 'No Darth Vader quote to delete'
+        }else{
+        window.location.reload()
+        }
+    })
+    .catch(error => console.error(error))
 })
--- server.js
+++ server.js
@@ -8,9 +8,9 @@ app.use(express.static('public'))
 
 MongoClient.connect('mongodb+srv://jadavila9:jayxx892@cluster0.ij5cahp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
     .then(client => {
-    console.log("Connected to Database")
+    console.log('Connected to Database')
     const db = client.db('quoteGenerator')
-    const quoteCollection = db.collection("quotes")
+    const quotesCollection = db.collection("quotes")
     
     app.use(bodyParser.urlencoded({extended: true}))
 
@@ -25,11 +25,27 @@ MongoClient.connect('mongodb+srv://jadavila9:jayxx892@cluster0.ij5cahp.mongodb.n
     })
     
     app.put('/quotes', (req, res) => {
-        console.log(req.body)
+        quotesCollection
+        .findOneAndUpdate({name: 'Yoda'},{
+            $set: {
+                name: req.body.name,
+                quote: req.body.quote,
+            },
+        }, 
+        {
+            upsert: true,
+        }
+        )
+        .then(result => {
+            res.json('Success')
+
+        })
+        .catch(error => console.error(error))
+
     })
 
     app.post('/quotes', (req, res) => {
-        quoteCollection
+        quotesCollection
             .insertOne(req.body)
             .then(result => {
                 res.redirect('/')
@@ -37,9 +53,23 @@ MongoClient.connect('mongodb+srv://jadavila9:jayxx892@cluster0.ij5cahp.mongodb.n
             .catch(error => console.error(error))
     })
 
+    app.delete('/quotes', (req, res) => {
+        quotesCollection
+        .deleteOne({name: req.body.name})
+        .then(result =>{
+            if(result.deletedCount === 0){
+                return res.json('No quote to delete')
+            }
+            res.json(`Deleted Darth Vader's quote`)
+        })
+        .catch(error => console.error(error))
+    })
+
+    
+    
+    
     app.set('view engine', 'ejs')
-
-
+    
     app.listen(3000, function(){
         console.log('listening on 3000')
     });
